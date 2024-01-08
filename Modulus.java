eae68aa0ebc62f1ed99ced3efed7d784295c0581
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-08 11:09:46 +0530

    import java.util.*; public class Hero{     public static void main(String[] sri){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         System.out.print(a%b);     } }

---
+++ Modulus.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hero{
+    public static void main(String[] sri){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a%b);
+    } 
+}
