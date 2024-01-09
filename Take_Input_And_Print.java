82b77ff08ccbf847b6c78b5f8740e055acd0bbad
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-09 21:24:17 +0530

    import java.util.*; public class Sri{     public static void main(String sri[]){         Scanner sc = new Scanner(System.in);         String sr = sc.nextLine();         System.out.print(sr);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Sri{
+    public static void main(String sri[]){
+        Scanner sc = new Scanner(System.in);
+        String sr = sc.nextLine();
+        System.out.print(sr);
+    }
+}
