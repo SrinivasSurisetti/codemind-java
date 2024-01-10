1bde647d2157e20d11bb397165f2f48930e14752
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-10 12:00:43 +0530

    import java.util.*; public class sri {     public static void main(String[] srg){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int c = sc.nextInt();         System.out.print(a/c);     } }

---
+++ Division.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri {
+    public static void main(String[] srg){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.print(a/c);
+    }
+}
