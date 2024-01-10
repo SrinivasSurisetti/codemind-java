e336f5f092c626c7ac06f0e836ab8d21f3a882b3
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-10 11:44:45 +0530

    import java.util.*; public class sri {     public static void main(String[] srg){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int c = sc.nextInt();         System.out.print((3*a)-(b+c));     } }

---
+++ Average_weight.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sri {
+    public static void main(String[] srg){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.print((3*a)-(b+c));
+    }
+}
