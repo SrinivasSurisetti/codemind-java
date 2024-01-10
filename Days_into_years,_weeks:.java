9ad3944d03feec57fded8e797d2f5f7f8b3036ec
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-10 11:54:35 +0530

    import java.util.*; public class sri {     public static void main(String[] srg){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         System.out.println(a/365);         System.out.print((a%365)/7);     } }

---
+++ Days_into_years,_weeks:.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri {
+    public static void main(String[] srg){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a/365);
+        System.out.print((a%365)/7);
+    }
+}
