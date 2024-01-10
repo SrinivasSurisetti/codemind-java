74108ed5ba10ddb4d81efc1fc7d504d67aa3fbcd
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-10 12:06:09 +0530

    import java.util.*; public class sri {     public static void main(String[] srg){         Scanner sc = new Scanner(System.in);         float a = sc.nextFloat();         float b = sc.nextFloat();         System.out.printf("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri {
+    public static void main(String[] srg){
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float b = sc.nextFloat();
+        System.out.printf("%.2f",a*b);
+    }
+}
