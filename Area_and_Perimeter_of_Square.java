b831e91ccacb9c7315aceb2e605c9ddf4f3329e1
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-11 12:00:34 +0530

    import java.util.*; public class s{     public static void main(String[] gdfds){         Scanner d=new Scanner(System.in);         int a = d.nextInt();         System.out.print((a*a)+" ");         System.out.print(4*a);     } }

---
+++ Area_and_Perimeter_of_Square.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class s{
+    public static void main(String[] gdfds){
+        Scanner d=new Scanner(System.in);
+        int a = d.nextInt();
+        System.out.print((a*a)+" ");
+        System.out.print(4*a);
+    }
+}
