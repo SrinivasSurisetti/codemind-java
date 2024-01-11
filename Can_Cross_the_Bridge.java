36d51db787072f6a719e949a933cf3b6e2e8cecd
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-11 11:36:40 +0530

    import java.util.*; public class s{     public static void main(String[] gdfds){         Scanner d=new Scanner(System.in);         int a = d.nextInt();         int b = d.nextInt();         int c = d.nextInt();         System.out.print((c-b)/a);     } }

---
+++ Can_Cross_the_Bridge.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class s{
+    public static void main(String[] gdfds){
+        Scanner d=new Scanner(System.in);
+        int a = d.nextInt();
+        int b = d.nextInt();
+        int c = d.nextInt();
+        System.out.print((c-b)/a);
+    }
+}
