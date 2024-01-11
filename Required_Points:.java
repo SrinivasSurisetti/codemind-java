3278e9c777ab60d7b9d76028589ba9f20f0213cc
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-11 11:27:05 +0530

    import java.util.*; public class s{     public static void main(String[] gdfds){         Scanner d=new Scanner(System.in);         int a = d.nextInt();         int b = d.nextInt();         if (a>b){             System.out.print(0);         }         else{         System.out.print(b/a);         }     } }

---
+++ Required_Points:.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class s{
+    public static void main(String[] gdfds){
+        Scanner d=new Scanner(System.in);
+        int a = d.nextInt();
+        int b = d.nextInt();
+        if (a>b){
+            System.out.print(0);
+        }
+        else{
+        System.out.print(b/a);
+        }
+    }
+}
