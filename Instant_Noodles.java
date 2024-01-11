54403e184767611a2b47772a5ecacfef26f8aaee
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-11 11:28:24 +0530

    import java.util.*; public class s{     public static void main(String[] gdfds){         Scanner d=new Scanner(System.in);         int a = d.nextInt();         int b = d.nextInt();         System.out.print(b*a);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class s{
+    public static void main(String[] gdfds){
+        Scanner d=new Scanner(System.in);
+        int a = d.nextInt();
+        int b = d.nextInt();
+        System.out.print(b*a);
+    }
+}
