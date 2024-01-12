4b6524fcd7d50205ad3ee06d49cf96a4dea20cd3
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-12 22:50:00 +0530

    Add files via upload

---
+++ Ternary_Operater.java
@@ -0,0 +1,11 @@
+public class Ternary_Operater{
+    public static void main(String[] sri) {
+        int a=10;
+        int b=19;
+        int max = a>b?a:b;
+        System.out.println(max);
+        System.out.println(a>b?a:b);
+        int n=10;
+        System.out.println(n%2==0?"Even" : "Odd");
+    }
+}
