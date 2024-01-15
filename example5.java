22cebb3a8a0d517f4c2a041d4a0fb6b1c3b0edf7
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-15 22:06:25 +0530

    Add files via upload

---
+++ example5.java
@@ -0,0 +1,13 @@
+class ABC{
+}
+class PQR{
+	public static void main(String[] args) {
+	System.out.print("RAVI");
+	}
+}
+class ex{
+	public static void main(String[] argwes){
+		int x = 10;
+		System.out.print(x);
+	}
+}
