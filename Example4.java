ce007a481afdda53e2bd55b0711b8004bb5eecec
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-16 21:06:57 +0530

    Add files via upload

---
+++ Example4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Example4 {
+	public  static void main(String[] arges) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		System.out.print("Your I/p is = ");
+		System.out.print(a);
+	}
+}
