1f2e545d280bd1c8bdd6b2489d89bfae381dc60a
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-19 22:00:01 +0530

    import java.util.*; public class r{     public static void main(String[] sri){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         int c = sc.nextInt();         System.out.print(2*a*b*c*512);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class r{
+    public static void main(String[] sri){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.print(2*a*b*c*512);
+    }
+}
