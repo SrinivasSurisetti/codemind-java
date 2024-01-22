767e7ee3ebee194a8835df4ae02080411b882ec3
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-22 11:37:27 +0530

    import java.util.*; public class sri{     public static void main(String[] sri){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         double b = 1.8*a+32;         System.out.printf("%.2f",b);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri{
+    public static void main(String[] sri){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = 1.8*a+32;
+        System.out.printf("%.2f",b);
+    }
+}
