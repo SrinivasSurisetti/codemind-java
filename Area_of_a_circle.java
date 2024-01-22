54830ae95c5e3270e419e720d034a4f7ab4c2f29
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-01-22 10:48:45 +0530

    import java.util.*; public class sri{     public static void main(String[] sri){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         double b = 3.14*a*a;         System.out.printf("%.2f",b);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri{
+    public static void main(String[] sri){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        double b = 3.14*a*a; 
+        System.out.printf("%.2f",b);
+    }
+}
