eeb7d5257cea0ec8c14405ba8172f0a976fd2f7c
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-02-01 11:16:30 +0530

    import java.util.*; public class srr{     public static void main(String[] srr){         Scanner d = new Scanner(System.in);         int a = d.nextInt();         int b = d.nextInt();         if (a*5>=b) System.out.print("YES");         else System.out.print("NO");     } }

---
+++ Reach_Home.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class srr{
+    public static void main(String[] srr){
+        Scanner d = new Scanner(System.in);
+        int a = d.nextInt();
+        int b = d.nextInt();
+        if (a*5>=b) System.out.print("YES");
+        else System.out.print("NO");
+    }
+}
