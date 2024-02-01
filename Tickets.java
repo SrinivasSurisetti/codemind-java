5b6e47cfb1eb7a0920fd8b338df6ed0b56e6cfbf
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-02-01 11:09:41 +0530

    import java.util.*; public class sri{ public static void main(String[] sri){     Scanner ni = new Scanner(System.in);     int a = ni.nextInt();     if((a*4)>1000) System.out.print("NO");     else System.out.print("YES"); } }

---
+++ Tickets.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri{
+public static void main(String[] sri){
+    Scanner ni = new Scanner(System.in);
+    int a = ni.nextInt();
+    if((a*4)>1000) System.out.print("NO");
+    else System.out.print("YES");
+}
+}
