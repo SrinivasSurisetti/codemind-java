6e906fef00a9673235d49248ce4140f73c57b344
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-02-05 10:05:40 +0530

    import java.util.*; public class sri{     public static void main(String[] sr){         Scanner s = new Scanner(System.in);         int  a = s.nextInt();         int b = s.nextInt();         System.out.print(Math.max(a,b));    } }

---
+++ Height.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class sri{
+    public static void main(String[] sr){
+        Scanner s = new Scanner(System.in);
+        int  a = s.nextInt();
+        int b = s.nextInt();
+        System.out.print(Math.max(a,b));
+   }
+}
