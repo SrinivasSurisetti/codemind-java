4a45ed9fecca7dcc6bf4cb45e4899b42a2393fc1
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-02-07 11:30:10 +0530

    import java.util.*; public class Sri{     public static void main(String[] sri){         Scanner s = new Scanner(System.in);         int a = s.nextInt();         int b = s.nextInt();         int c = s.nextInt();         System.out.print(Math.max(a,Math.max(b,c)));     } }

---
+++ Largest_number_among_3_numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sri{
+    public static void main(String[] sri){
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int c = s.nextInt();
+        System.out.print(Math.max(a,Math.max(b,c)));    
+    }
+}
